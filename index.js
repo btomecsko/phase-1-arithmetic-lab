649f3347cc5a23362c4ac22d755dcf00ad8503c6
btomecsko <brandon.tomecsko92@gmail.com>
2022-03-15 17:56:47 -0500

    commit tree 5cde0a3b1d36eb6f059e11e7ef1afe3f00374e5d

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 1;
+let num2 = 62;
+const multiply = num1*num2;
+let random = Math.ceil(Math.random()* 10);
+let num3 = 19;
+let num4 = 5;
+const mod = num3%num4;
+let max = Math.max(5, 10, 15, 20);
 
 // Write your code here
